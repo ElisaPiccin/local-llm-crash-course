5e967ec06f2023f8425d194acdeaac6fdd589f80
Elisa <piccin.elisa@gmail.com>
2024-07-30 10:40:05 +0000

    llama 2 exercise

--- chat_llama2.py
+++ chat_llama2.py
@@ -1,6 +1,6 @@
 from ctransformers import AutoModelForCausalLM
 
-llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
+llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7b-Chat-GGUF", model_file="llama-2-7b-chat.Q5_K_M.gguf")
 
 # this model cannot still chat, it is able to complete sentences
 # [VIDEO EXAMPLE]
@@ -23,8 +23,8 @@ prompt = "The name of capital of India is"
 
 def get_prompt(instruction: str) -> str:
     # system = "You are an AI assistant that follows instruction extremely well. Help as much as you can. Give short answers."
-    system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
-    prompt = f"### System:\n{system}\n\n### User:\n{instruction}\n\n### Response:\n"
+    system = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
+    prompt = f"<s>[INST] <<SYS>>\n{system}\n<</SYS>>\n\n{instruction} [/INST]"
     print(prompt)
     return prompt
 
